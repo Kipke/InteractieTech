95f029138fe738024761eaa030959d14df240014
ReinierMaas <reiniermaas12345@gmail.com>
2015-02-26 19:46:57 +0100

    Nog een laatste distance dingetje

--- InteractieTech/number_oneActions.ino
+++ InteractieTech/number_oneActions.ino
@@ -12,9 +12,8 @@ void number_oneActions(){
 
   // TODO: if toilet paper used
   //   tpUsed = true;
-  if (millis() >= pingTimer) {   // pingSpeed milliseconds since last ping, do another ping.
-    pingTimer += pingSpeed;      // Set the next ping time.
-    sonar.ping_timer(echoCheck); // Send out the ping, calls "echoCheck" function every 24uS where you can check the ping status.
+  if (lastDistance < baselineDistance / 2) {
+    //TODO iets alsde distance change has occured
   }
   // if time > y and tpUsed
   //   state = NUMBER_TWO;
