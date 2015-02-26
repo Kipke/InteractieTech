261242ca3e78a2ad8f94503e2e757d6175f02cc2
Kipke <bjorn.molenmaker@gmail.com>
2015-02-26 19:29:26 +0100

    Distance sensor proof of concept
    Again, now using a pin interrupt. Still uses 2 pins but that should not
    be a problem.

--- AfstandSensor/AfstandSensor.ino
+++ AfstandSensor/AfstandSensor.ino
@@ -1,35 +1,29 @@
-#include <NewPing.h>
-
-#define TRIGGER_PIN  7  // Arduino pin tied to trigger pin on ping sensor.
-#define ECHO_PIN     11  // Arduino pin tied to echo pin on ping sensor.
-#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
-
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
-
-unsigned int pingSpeed = 200; // How frequently are we going to send out a ping (in milliseconds). 50ms would be 20 times a second.
-unsigned long pingTimer;     // Holds the next ping time.
-
+const int distanceSensorTrigger = 3;
+const int distanceSensorEcho = 11;
+int distance = 0;
+int timeout = 2500 // in microseconds
 void setup() {
-  Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
-  pingTimer = millis(); // Start now.
+  Serial.begin(9600);
+  
+  pinMode(distanceSensorEcho, INPUT);
+  pinMode(distanceSensorTrigger, OUTPUT);
+  attachInterrupt(1,measureDistance,CHANGE);
 }
 
-void loop() {
-  // Notice how there's no delays in this sketch to allow you to do other processing in-line while doing distance pings.
-  if (millis() >= pingTimer) {   // pingSpeed milliseconds since last ping, do another ping.
-    pingTimer += pingSpeed;      // Set the next ping time.
-    sonar.ping_timer(echoCheck); // Send out the ping, calls "echoCheck" function every 24uS where you can check the ping status.
-  }
-  // Do other stuff here, really. Think of it as multi-tasking.
+void loop()
+{
+  digitalWrite(distanceSensorTrigger,HIGH);
+  delay(10);
+  digitalWrite(distanceSensorTrigger,LOW);  
+  Serial.println(distance);  
 }
 
-void echoCheck() { // Timer2 interrupt calls this function every 24uS where you can check the ping status.
-  // Don't do anything here!
-  if (sonar.check_timer()) { // This is how you check to see if the ping was received.
-    // Here's where you can add code.
-    Serial.print("Ping: ");
-    Serial.print(sonar.ping_result / US_ROUNDTRIP_CM); // Ping returned, uS result in ping_result, convert to cm with US_ROUNDTRIP_CM.
-    Serial.println("cm");
-  }
-  // Don't do anything here!
+void measureDistance(){
+    if(digitalRead(distanceSensorTrigger)){
+      int t = pulseIn(distanceSensorEcho, HIGH,timeout);      
+      distance =  t / 29 / 2;       
+    }
+    
 }
+
+
