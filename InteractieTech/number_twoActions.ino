816fd7e47c339f9d8cd73f24674c3fb1af441e2d
Kipke <bjorn.molenmaker@gmail.com>
2015-02-27 15:45:49 +0100

    Properly indented the code etc.
    Also cleaned up most of the useless comments
    
    Closes #25

--- InteractieTech/number_twoActions.ino
+++ InteractieTech/number_twoActions.ino
@@ -1,16 +1,19 @@
+// Reinier Maas     - 4131495
+// Björn Molenmaker - 3843874
+
 void number_twoActions(){
-    // light is red
-    lightColor(255,0,0);
-    if(!doorClosed){
-      doorWasOpen = true;
-    }
-    // if door opens and no movement
-    if(doorWasOpen && !motionDetected){
-       shotsToFire = 2; 
-       // set state to triggered
-       state = TRIGGERED;
-       // start the degradation timer
-       degradationTime = millis();
-       doorWasOpen = false;
-    } 
+	// light is red
+	lightColor(255,0,0);
+	if(!doorClosed){
+		doorWasOpen = true;
+	}
+	// if door opens and no movement
+	if(doorWasOpen && !motionDetected){
+		shotsToFire = 2; 
+		// set state to triggered
+		state = TRIGGERED;
+		// start the degradation timer
+		degradationTime = millis();
+		doorWasOpen = false;
+	} 
 }
--- InteractieTech/triggeredActions.ino
+++ InteractieTech/triggeredActions.ino
@@ -1,50 +1,49 @@
+// Reinier Maas     - 4131495
+// Björn Molenmaker - 3843874
+
 int pulses = 0;
 int pulseElapsed = shotDelay / 10;;
 void triggeredActions(){
-  // light is flashing color of shots remaining
-  // degradation timer and shots to fire magic :-)
-  // if no movement
-  if(shotsToFire == 0){
-     state = STANDBY;
-     return; 
-  }
-  if(shotsToFire == 1){
-     // light is yellow
-     lightColor(255,255,0); 
-  }
-  //   start shot countdown
-  if(shotTime == -1){
-     shotTime = millis();
-  }
-  // flip the light
-  if(timeElapsed(shotTime,pulseElapsed)){     
-     flipLight(); 
-     int pulseSpeed = shotDelay / (10 + 3 * pulses); 
-     pulses++;     
-     if(pulseSpeed < 50){
-        pulseSpeed = 50;
-     }
-     pulseElapsed += pulseSpeed;
-  }
-  //  float temp2 = temp / shotDelay;  
-  //  lightIntensity(temp2);
-  //  int lightIntensity = temp2 * 255;
-  //  lightColor(lightIntensity,lightIntensity,lightIntensity);
-  
-  // fire the shot if neccesary
-  if (timeElapsed(shotTime,shotDelay) && shotTime != (-1)){    
-    digitalWrite(actuator,HIGH);
-    delay(2000);
-    digitalWrite(actuator,LOW);
-    shotsToFire--;
-    // update the shotRemaining
-    shotsRemaining--;
-    // update the EEPROM
-    // writeToEEPROM(shotsRemaining);
-    // Reset the pulse settings for the next shot
-    shotTime = -1;
-    pulses = 1;
-    pulseElapsed = shotDelay / 10;
-    lightIntensity(1);
-  }
+	// light is flashing color of shots remaining
+	// Shots to fire magic
+	// if no more shots left to fire, go to standby
+	if(shotsToFire == 0){
+		state = STANDBY;
+		return; 
+	}
+        // if there is only one shot left, the light should be yellow
+	if(shotsToFire == 1){
+		// light is yellow
+		lightColor(255,255,0); 
+	}
+	//   start shot countdown if necessary
+	if(shotTime == -1){
+		shotTime = millis();
+	}
+	// flip the light
+	if(timeElapsed(shotTime,pulseElapsed)){     
+		flipLight(); 
+		int pulseSpeed = shotDelay / (10 + 3 * pulses); 
+		pulses++;     
+		if(pulseSpeed < 50){
+			pulseSpeed = 50;
+		}
+		pulseElapsed += pulseSpeed;
+	}
+	// fire the shot if neccesary
+	if (timeElapsed(shotTime,shotDelay) && shotTime != (-1)){    
+		digitalWrite(actuator,HIGH);
+		delay(2000);
+		digitalWrite(actuator,LOW);
+		shotsToFire--;
+		// update the shotRemaining
+		shotsRemaining--;
+		// update the EEPROM
+		// writeToEEPROM(shotsRemaining);
+		// Reset the pulse settings for the next shot
+		shotTime = -1;
+		pulses = 1;
+		pulseElapsed = shotDelay / 10;
+		lightIntensity(1);
+	}
 }
