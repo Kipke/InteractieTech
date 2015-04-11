6b15ffd0d224cc679fe7496bc2f271f00f5d59ee
ReinierMaas <reiniermaas12345@gmail.com>
2015-04-11 16:48:57 +0200

    Update write eeprom

--- InteractieTech/helperFunctions.ino
+++ InteractieTech/helperFunctions.ino
@@ -93,26 +93,10 @@ void processLed(){
 
 void writeToEEPROM(int number){
 	// a base 256 number system is used to write to the EEPROM, because each EEPROM index can hold 256 values(it is a byte).
-	// if the number is smaller than 256  
-	if(number < 256){
-		// write a 0 at index 0 for safety 
-		EEPROM.write(0,0);
-		// and write the number at index 1
-		EEPROM.write(1,number); 
-	}
-	int mod = number % 256;
-	if(mod == 0){
-		// write the number divided by 256 at index 0
-		EEPROM.write(0,number / 256);
-		// write a 0 at index 1 for safety
-		EEPROM.write(1,0);
-		return; 
-	}
-	number - mod;
 	// write what remains of the number, divided by 256 at index 0
 	EEPROM.write(0,number / 256);
-	// write what was left over at index 1
-	EEPROM.write(1,mod);
+	// write what is left over at index 1
+	EEPROM.write(1,number % 256);
 }
 
 int readFromEEPROM(){
